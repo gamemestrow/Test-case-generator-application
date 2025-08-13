abf8e7f70a602b9f2eda25a482774aa977ad34ff
Ayush Kumar Mahto <mak62mes176trow@gmail.com>
2025-08-13 09:49:36 +0530

    code after test file

---
+++ backend/dict.py
@@ -0,0 +1,11 @@
+d1 = {
+    "apple" : "fruit"
+   , "Banana" : "also a fruit"
+   , "potato" : "vegetable"
+    , "carrot" : "vegetable"
+}
+
+print("what do you want to know")
+m1 = input()
+
+print(d1[m1])
